06c57912491d53e6433f5919f68f63be02308518
Alyssa Nicoll <alyssaamnell@gmail.com>
2020-01-31 15:20:50 -0600

    fix tsconfig path and add product alert comp

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-product-alerts',
+  selector: 'product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ProductAlertsComponent {
+  @Input() product;
+  @Output() notify = new EventEmitter();
+  
+  constructor() { 
+    this.notify;
   }
-
 }
